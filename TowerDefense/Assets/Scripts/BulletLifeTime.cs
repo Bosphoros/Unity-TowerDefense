b64745ec438d2395e523377359b37e1578fb2266
Bosphoros <floriantd.adam@gmail.com>
2015-11-18 00:47:45 +0100

    Init

---
+++ TowerDefense/Assets/Scripts/BulletLifeTime.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletLifeTime : MonoBehaviour {
+
+	private int lifeTime = 300;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifeTime--;
+		if (lifeTime <= 0) {
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ TowerDefense/Assets/Scripts/Levitation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Levitation : MonoBehaviour {
+
+	public int decalageTime ;
+	private int time = 0 ;
+	public enum rotationDirection : int {RIGHT = -1, NONE = 0, LEFT = 1};
+	public rotationDirection rotation;
+	public float rotationSpeed;
+	public float levitationHeight;
+
+	// Use this for initialization
+	void Start () {
+		time += decalageTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time++;
+		this.transform.Rotate (Time.fixedDeltaTime * Vector3.up * rotationSpeed * (int) rotation);
+		transform.Translate (Time.fixedDeltaTime * Mathf.Cos (time/60) * Vector3.up * levitationHeight);
+	}
+}
